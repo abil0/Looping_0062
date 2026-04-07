510b0d5b622fc0457ef97b3b9ac09aee49b4f3df
abil0 <hoenabil6@gmail.com>
2026-04-07 10:29:52 +0700

    not_logic status all

--- not_logic.cpp
+++ not_logic.cpp
@@ -10,4 +10,15 @@ int main() {
     cout << "Masukkan Nilai Matematika : ";
     cin >> nilM;
     
+    rerata = (nilB + nilM) /2;
+
+    if (!(rerata < 60))
+     
+    status = "Lulus";
+    else
+    status = "Tidak Lulus";
+
+    cout << "status kelulusan : " << status << "Dengan nilai rerata : " << rerata << endl;
+
+    return 0;
 }
