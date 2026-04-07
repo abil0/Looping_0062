11eef07ae13618b654a6b0991d7a4c2cd5832e8a
abil0 <hoenabil6@gmail.com>
2026-04-07 10:44:18 +0700

    input jurusan

--- switchcase.cpp
+++ switchcase.cpp
@@ -4,3 +4,13 @@ using namespace std;
 int kodejurusan;
 string namajurusan;
 string statuskuota;
+
+void input() {
+    cout << "===== PENDAFTARAN MAHASISWA BARU =====" << endl;
+    cout << "1. teknologi informasi ";
+    cout << "2. teknik elektro";
+    cout << "3. sistem informasi";
+    cout << "------------------------------" << endl;
+    cout << "Masukkan Kode Jurusan (1-3): ";
+    cin >> kodejurusan;
+}
