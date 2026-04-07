8d88f67d54c7fa93f19cc585bf3fb2bbcc266e8e
abil0 <hoenabil6@gmail.com>
2026-04-07 10:38:00 +0700

    and logic review

---
+++ and_logic.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float nilB, nilM, rerata;
+    char predikat;
+
+    cout << "Masukkan Nilai Bahasa inggris : ";
+    cin >> nilB;
+    cout << "Masukkan Nilai Matematika : ";
+    cin >> nilM;
+    
+    rerata = (nilB + nilM) /2;
+
+    if ((rerata >= 60) && (nilM >= 70))
+    status = "Lulus";
+    else
+    status = "Tidak Lulus";
+
+    cout << "status kelulusan : " << status << "Dengan nilai rerata : " << rerata << endl;
+
+    return 0;
+}
