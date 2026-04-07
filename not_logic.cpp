710a4a6af90b72f31807bba255ec06b60f9d1f92
abil0 <hoenabil6@gmail.com>
2026-04-07 10:22:38 +0700

    not_logic cpp status review

---
+++ not_logic.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float nilB, nilM, rerata;
+    char predikat;
+
+    cout << "Masukkan Nilai Bahasa inggris : ";
+    cin >> nilB;
+    cout << "Masukkan Nilai Matematika : ";
+    cin >> nilM;
+    
+}
